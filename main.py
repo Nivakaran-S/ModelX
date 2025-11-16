02917f82adf86e30c92d2c792fb0a76231579a66
sliitguy <it23259416@my.sliit.lk>
2025-11-16 18:20:32 +0530

    updated data transformation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
-# from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformation
 # from src.components.model_trainer import ModelTrainer
 from src.exception.exception import DementiaException
 from src.logging.logger import logging
@@ -27,12 +27,12 @@ if __name__=='__main__':
         print(data_validation_artifact)
 
 
-        # data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
-        # logging.info("data Transformation started")
-        # data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
-        # data_transformation_artifact=data_transformation.initiate_data_transformation()
-        # print(data_transformation_artifact)
-        # logging.info("data Transformation completed")
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
 
         # logging.info("Model Training stared")
         # model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
